2ecd34010375f83f47cfa012b3ff21c3d205111d
ramiro.rela <ramiro.rela@c89c9ce8-ad64-11de-b127-75e66c4542a6>
2009-10-09 18:22:09 +0000

    Improves polygloth greetings with deep_test

--- ruby/deep_test/simple_test/tests/hallo_test.rb
+++ ruby/deep_test/simple_test/tests/hallo_test.rb
@@ -1,1 +1,17 @@
-puts "Hello vom #{Process.pid}"
+require "tests/hello"
+
+class GermanHello < Hello
+  def hello
+    "Hallo"
+  end
+
+  def goodbye
+    "Tschuss"
+  end
+
+  def lang
+    "ge"
+  end
+end
+
+GermanHello.new
